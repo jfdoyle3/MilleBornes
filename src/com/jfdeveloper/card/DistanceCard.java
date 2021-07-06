6cae0f3765ab60703d1308904ce6385d77904278
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-07-05 21:18:09 -0400

    thinking of way to use enum for cards

--- src/com/jfdeveloper/card/DistanceCard.java
+++ src/com/jfdeveloper/card/DistanceCard.java
@@ -3,7 +3,8 @@ package com.jfdeveloper.card;
 public enum DistanceCard {
 
     TWENTYFIVE("10M"),
-    FIFTY("50M");
+    FIFTY("50M"),
+	D25 ("25 Miles", "25.png", "|25|"),;
 
     private String cardValue;
 
