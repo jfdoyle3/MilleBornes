eb369f6b765addb71f9b8ca71696dba7059cce4d
jfdoyle3 <indellibleink@gmail.com>
2022-11-24 18:55:24 -0500

    building hand class

--- src/com/jfdeveloper/hand/Hand.java
+++ src/com/jfdeveloper/hand/Hand.java
@@ -2,5 +2,20 @@ package com.jfdeveloper.hand;
 
 public class Hand {
 
+    public Hand(Actor holder) {
+        this.holder = holder;
+    }
 
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public String displayHand() {
+        StringBuilder output = new StringBuilder();
+        for (Card card : cards) {
+            output.append(card.display()).append(" ");
+        }
+
+        return output.toString().trim();
+    }
 }
