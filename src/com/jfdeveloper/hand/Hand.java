12eea1f2d2d751cb30e35a72d48c1d6fff330b2d
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-11-29 22:24:14 -0500

    Working on hand class

--- src/com/jfdeveloper/hand/Hand.java
+++ src/com/jfdeveloper/hand/Hand.java
@@ -1,17 +1,21 @@
 package com.jfdeveloper.hand;
 
+import com.jfdeveloper.card.Card;
+
 public class Hand {
 
-    public Hand(Actor holder) {
-        this.holder = holder;
-    }
+//    public Hand(Actor holder) {
+//        this.holder = holder;
+//    }
 
     public void addCard(Card card) {
+        java.lang.Object cards = null;
         cards.add(card);
     }
 
     public String displayHand() {
         StringBuilder output = new StringBuilder();
+        Card[] cards;
         for (Card card : cards) {
             output.append(card.display()).append(" ");
         }
